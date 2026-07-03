f2f541b9566b32d22cd0a022d60c5939c17deb75
marcoromani-png <marco.romani@accenture.com>
2026-07-03 18:00:28 +0200

    Add getAllEpisodes

---
+++ vars/getAllEpisodes.groovy
@@ -0,0 +1,31 @@
+import groovy.json.JsonSlurperClassic
+
+def call(String startUrl) {
+    //Lista vuota dove saranno messi tutti gli episodi di tutte le pagine.
+    def allEpisodes = []
+    //Inizio della prima URL
+    def currentUrl = startUrl
+    //Continuo a chiamare l’API finché esiste una pagina successiva.
+    while (currentUrl != null) {
+        echo "Chiamo API: ${currentUrl}"
+
+        def response = httpRequest(
+            httpMode: 'GET',
+            url: currentUrl,
+            validResponseCodes: '200',
+            contentType: 'APPLICATION_JSON'
+        )
+
+        def json = new JsonSlurperClassic().parseText(response.content)
+
+        if (json.results != null) {
+            allEpisodes.addAll(json.results)
+        }
+        //Prendo la prossima pagina. Se non esiste, diventa null e il ciclo finisce.
+        currentUrl = json.info?.next_page
+
+        echo "Episodi raccolti finora: ${allEpisodes.size()}"
+    }
+
+    return allEpisodes
+}
